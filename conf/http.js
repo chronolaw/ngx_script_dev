c19ae432762d2084ebc344ebcf5fa4980f0ed4b7
chronolaw <chrono_cpp@me.com>
2018-12-25 11:38:36 +0800

    js conf

---
+++ conf/http.js
@@ -0,0 +1,14 @@
+// Copyright (C) 2018 by chrono
+
+// a array
+var strs = ['hello', 'nginx', 'nginScript'];
+
+// test log
+function test_log(r)
+{
+    r.log("info levelmessage");
+    r.error("hello Nginx JavaScript");
+
+    return "hello JavaScript";
+}
+
---
+++ conf/stream.js
@@ -0,0 +1,12 @@
+// Copyright (C) 2017 by chrono
+
+// a array
+var strs = ['hello', 'nginx', 'nginScript'];
+
+// a var set
+function hello(s)
+{
+    s.log("stream nginScript");
+    return "stream nginScript";
+}
+
